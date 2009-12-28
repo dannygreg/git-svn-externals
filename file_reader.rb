984aecfa4999ab9b279e6cc321db60ac039c234d
Danny Greg <danny@realmacsoftware.com>
2009-12-28 21:56:49 +0000

    Added in file_reader.rb and tried to get it to require… I'm such a n00b.

---
+++ file_reader.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+include FileUtils
+
+class FileReader
+
+	def FileReader.read_file(File file) 
+		return_string = String.new
+		while (!file.eof?) do
+			return_string += file.read(32)
+		end
+		return_string
+	end
+
+end
